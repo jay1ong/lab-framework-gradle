3b46c2e0cde45cca9c2cbd83c1b3f426dd402610
I'm <625013913@qq.com>
2021-12-14 09:50:00 +0800

    refactor: 更改镜像源为腾讯私有仓库提速

--- lab-test-gradle/src/main/java/cn/jaylong/test/controller/TestController.java
+++ lab-test-gradle/src/main/java/cn/jaylong/test/controller/TestController.java
@@ -6,9 +6,6 @@ import cn.jaylong.test.model.UserValidatorModel;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.axonframework.common.IdentifierFactory;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.Errors;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
